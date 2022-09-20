d8f8b1cd983cbd792a074473d12316e413931e2f
kongdywang <kongdywang@tencent.com>
2022-09-20 20:05:07 +0800

    fix superPlayer slider click invalid issue

--- Flutter/example/lib/superplayer/ui/superplayer_widget.dart
+++ Flutter/example/lib/superplayer/ui/superplayer_widget.dart
@@ -84,7 +84,8 @@ class SuperPlayerViewState extends State<SuperPlayerView> with WidgetsBindingObs
       }
     });
     // only register listen once
-    _pipSubscription = SuperPlayerPlugin.instance.onExtraEventBroadcast.listen((event) {
+    _pipSubscription =
+        SuperPlayerPlugin.instance.onExtraEventBroadcast.listen((event) {
       int eventCode = event["event"];
       if (eventCode == TXVodPlayEvent.EVENT_PIP_MODE_ALREADY_EXIT) {
         // exit floatingMode
@@ -126,7 +127,8 @@ class SuperPlayerViewState extends State<SuperPlayerView> with WidgetsBindingObs
         print('$eventCode');
       }
     });
-    _volumeSubscription = SuperPlayerPlugin.instance.onEventBroadcast.listen((event) {
+    _volumeSubscription =
+        SuperPlayerPlugin.instance.onEventBroadcast.listen((event) {
       int eventCode = event["event"];
       if (_isFloatingMode && _isPlaying) {
         if (eventCode == TXVodPlayEvent.EVENT_AUDIO_FOCUS_PAUSE) {
