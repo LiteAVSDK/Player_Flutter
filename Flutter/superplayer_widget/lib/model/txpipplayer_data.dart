86800444172c81c399c7f67cb3c3ea523ad0d5d3
kongdywang <kongdywang@tencent.com>
2022-12-08 17:09:53 +0800

    fix some pip bug & modify docs

--- Flutter/superplayer_widget/lib/model/txpipplayer_data.dart
+++ Flutter/superplayer_widget/lib/model/txpipplayer_data.dart
@@ -6,6 +6,7 @@ class TXPipPlayerData {
   /// 画中画播放器实例，同时只能存在一个
   final TXPlayerController _playerController;
   int playerMode = TXPlayerType.VOD_PLAY;
+  bool isEnterPip = false;
 
   TXPipPlayerData(this._playerController) {
     playerMode = _playerController is TXVodPlayerController ? TXPlayerType.VOD_PLAY : playerMode = TXPlayerType.LIVE_PLAY;
