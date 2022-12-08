1890aaa58a74c8261e5b65cf74023fdcc4de799e
kongdywang <kongdywang@tencent.com>
2022-12-08 15:43:52 +0800

    fix ios progressInterval invalid

--- Flutter/ios/Classes/FTXTransformation.m
+++ Flutter/ios/Classes/FTXTransformation.m
@@ -15,7 +15,6 @@
     playConfig.enableAccurateSeek = [args[@"config"][@"enableAccurateSeek"] boolValue];
     playConfig.autoRotate = [args[@"config"][@"autoRotate"] boolValue];
     playConfig.smoothSwitchBitrate = [args[@"config"][@"smoothSwitchBitrate"] boolValue];
-    playConfig.progressInterval = [args[@"config"][@"progressInterval"] intValue];
     playConfig.maxBufferSize = [args[@"config"][@"maxBufferSize"] intValue];
     playConfig.maxPreloadSize = [args[@"config"][@"maxPreloadSize"] intValue];
     playConfig.firstStartPlayBufferTime = [args[@"config"][@"firstStartPlayBufferTime"] intValue];
@@ -24,6 +23,11 @@
     
     NSString *preferredResolutionStr = args[@"config"][@"preferredResolution"];
     playConfig.preferredResolution = [preferredResolutionStr longLongValue];
+    
+    NSTimeInterval progressInerval = [args[@"config"][@"progressInterval"] intValue] / 1000.0;
+    if(progressInerval > 0) {
+        playConfig.progressInterval = progressInerval;
+    }
     
     NSString *overlayKey =  args[@"config"][@"overlayKey"];
     if(overlayKey != nil && overlayKey.length > 0) {
