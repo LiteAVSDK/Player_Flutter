74b5fed67b4f521a6fa63cc09aa285687539e7ba
kongdywang <kongdywang@tencent.com>
2023-05-11 15:56:47 +0800

    Fixing the issue of event disorder in multiple lists of short videos & remove third depend eventBus

--- Flutter/example/lib/shortvideo/demo_short_video_lib.dart
+++ Flutter/example/lib/shortvideo/demo_short_video_lib.dart
@@ -8,8 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:super_player/super_player.dart';
 import 'package:superplayer_widget/demo_superplayer_lib.dart';
 import 'package:super_player_example/ui/demo_video_slider_view.dart';
-import 'package:event_bus/event_bus.dart';
 
 part 'model/short_video_data_loader.dart';
 part 'short_video_page_widget.dart';
-part 'tools/event_bus_utils.dart';
+part 'event/short_video_event.dart';
+part 'event/video_event_dispatcher.dart';
