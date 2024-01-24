cbef6e07df3d4d7411d0ba64de3fbe3842e29232
kongdywang <kongdywang@tencent.com>
2024-01-24 17:49:37 +0800

    1. superPlayerWidget has added ability to config video render mode 2. prevent app ios crash when kill 3. fix issue that vod player can not pick width

--- Flutter/lib/Core/txliveplayer_controller.dart
+++ Flutter/lib/Core/txliveplayer_controller.dart
@@ -78,15 +78,17 @@ class TXLivePlayerController extends ChangeNotifier implements ValueListenable<T
         break;
       case TXVodPlayEvent.PLAY_EVT_CHANGE_RESOLUTION: //下行视频分辨率改变
         if (defaultTargetPlatform == TargetPlatform.android) {
-          double? videoWidth = (event["videoWidth"]);
-          double? videoHeight = (event["videoHeight"]);
+          int? videoWidth = event[TXVodPlayEvent.EVT_VIDEO_WIDTH];
+          int? videoHeight = event[TXVodPlayEvent.EVT_VIDEO_HEIGHT];
+          videoWidth ??= event[TXVodPlayEvent.EVT_PARAM1];
+          videoHeight ??= event[TXVodPlayEvent.EVT_PARAM2];
           if ((videoWidth != null && videoWidth > 0) && (videoHeight != null && videoHeight > 0)) {
-            resizeVideoWidth = videoWidth;
-            resizeVideoHeight = videoHeight;
-            videoLeft = event["videoLeft"];
-            videoTop = event["videoTop"];
-            videoRight = event["videoRight"];
-            videoBottom = event["videoBottom"];
+            resizeVideoWidth = videoWidth.toDouble();
+            resizeVideoHeight = videoHeight.toDouble();
+            videoLeft = event["videoLeft"] ?? 0;
+            videoTop = event["videoTop"] ?? 0;
+            videoRight = event["videoRight"] ?? 0;
+            videoBottom = event["videoBottom"] ?? 0;
           }
         }
         break;
