5b64ede8031fde3330a0400ed2c4944023865d7e
kongdywang <kongdywang@tencent.com>
2024-05-24 10:48:45 +0800

    提交漏提交文件TXCommonUtil (merge request !297)
    Squash merge branch 'kongdywang/fix/fix_txcommon_miss' into 'main'
    提交漏提交文件TXCommonUtil

---
+++ Flutter/ios/Classes/tools/TXCommonUtil.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Tencent. All rights reserved.
+
+#import <Foundation/Foundation.h>
+#import "FTXEvent.h"
+#import "FtxMessages.h"
+
+@interface TXCommonUtil : NSObject
+
++(NSNumber*)getDownloadEventByState:(int)downloadState;
+
++(PlayerMsg*)playerMsgWith:(NSNumber*)playerId;
+
++(StringMsg*)stringMsgWith:(NSString*)str;
+
++(DoubleMsg*)doubleMsgWith:(double)value;
+
++(BoolMsg*)boolMsgWith:(bool)value;
+
++(IntMsg*)intMsgWith:(NSNumber*)value;
+
++(UInt8ListMsg*)uInt8MsgWith:(NSData*)value;
+
++(ListMsg*)listMsgWith:(NSArray*)value;
+
+@end
---
+++ Flutter/ios/Classes/tools/TXCommonUtil.m
@@ -0,0 +1,89 @@
+// Copyright (c) 2022 Tencent. All rights reserved.
+
+#import "TXCommonUtil.h"
+#import <Flutter/Flutter.h>
+#include "FTXLiteAVSDKHeader.h"
+
+#if __has_include(<TXLiteAVSDK_Player/TXVodDownloadManager.h>)
+#import <TXLiteAVSDK_Player/TXVodDownloadManager.h>
+#elif __has_include(<TXLiteAVSDK_Player_Premium/TXVodDownloadManager.h>)
+#import <TXLiteAVSDK_Player_Premium/TXVodDownloadManager.h>
+#elif __has_include(<TXLiteAVSDK_Professional/TXVodDownloadManager.h>)
+#import <TXLiteAVSDK_Professional/TXVodDownloadManager.h>
+#else
+#import <TXVodDownloadManager.h>
+#endif
+
+
+@implementation TXCommonUtil
+
++ (NSNumber*)getDownloadEventByState:(int)downloadState{
+    int result;
+    switch (downloadState) {
+        case TXVodDownloadMediaInfoStateInit:
+            result = EVENT_DOWNLOAD_START;
+            break;
+        case TXVodDownloadMediaInfoStateStart:
+            result = EVENT_DOWNLOAD_PROGRESS;
+            break;
+        case TXVodDownloadMediaInfoStateStop:
+            result = EVENT_DOWNLOAD_STOP;
+            break;
+        case TXVodDownloadMediaInfoStateError:
+            result = EVENT_DOWNLOAD_ERROR;
+            break;
+        case TXVodDownloadMediaInfoStateFinish:
+            result = EVENT_DOWNLOAD_FINISH;
+            break;
+        default:
+            result = EVENT_DOWNLOAD_ERROR;
+            break;
+    }
+    return [NSNumber numberWithInt:result];;
+}
+
++(PlayerMsg*)playerMsgWith:(NSNumber*)playerId {
+    PlayerMsg* msg = [[PlayerMsg alloc] init];
+    msg.playerId = playerId;
+    return msg;
+}
+
++(StringMsg*)stringMsgWith:(NSString*)str {
+    StringMsg *msg = [[StringMsg alloc] init];
+    msg.value = str;
+    return msg;
+}
+
++(DoubleMsg*)doubleMsgWith:(double)value {
+    DoubleMsg *msg = [[DoubleMsg alloc] init];
+    msg.value = @(value);
+    return msg;
+}
+
++(BoolMsg *)boolMsgWith:(bool)value {
+    BoolMsg *msg = [[BoolMsg alloc] init];
+    msg.value = @(value);
+    return msg;
+}
+
++ (IntMsg *)intMsgWith:(NSNumber *)value {
+    IntMsg *msg = [[IntMsg alloc] init];
+    msg.value = value;
+    return msg;
+}
+
++ (UInt8ListMsg *)uInt8MsgWith:(NSData *)value {
+    UInt8ListMsg *msg = [[UInt8ListMsg alloc] init];
+    if(nil != value) {
+        msg.value = [FlutterStandardTypedData typedDataWithBytes:value];
+    }
+    return msg;
+}
+
++ (ListMsg *)listMsgWith:(NSArray *)value {
+    ListMsg *msg = [[ListMsg alloc] init];
+    msg.value = value;
+    return msg;
+}
+
+@end
